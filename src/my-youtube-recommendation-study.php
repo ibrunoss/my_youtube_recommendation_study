edc9e490b9de489308dcee70f617a60de92ab152
Bruno Silva Santana <ibrunoss@outlook.com>
2021-04-17 19:22:38 -0300

    Files structure created

---
+++ src/my-youtube-recommendation-study.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @link              https://github.com/ibrunoss/my_youtube_recommendation_study
+ * @since             1.0.0
+ * @package           My_Youtube_Recommendation_Study
+ * 
+ * @wordpress-plugin
+ * Plugin Name:       My Youtube Recommendation Study
+ * Plugin URI:        https://github.com/ibrunoss/my_youtube_recommendation_study
+ * Description:       Display the last videos from a Youtube channel using Youtube feed and keep always updated even for cached posts.
+ * Version:           1.0.0
+ * Author:            Bruno Silva Santana
+ * Author URI:        https://github.com/ibrunoss
+ * License:           GPLv3 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:       my-youtube-recommendation-study
+ * Domain Path:       /languages/
+ */
