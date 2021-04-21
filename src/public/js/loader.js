8ff35c7915dd9b8327349000e0c0354d5b7f7bb2
Bruno Silva Santana <ibrunoss@outlook.com>
2021-04-20 23:00:21 -0300

    Loader fix, wait jquery

--- src/public/js/loader.js
+++ src/public/js/loader.js
@@ -1,5 +1,9 @@
-MyYoutubeRecommendationStudy.loadVideos(my_yt_rec_s_ajax.url).then((value) => {
-  MyYoutubeRecommendationStudy.listCallbacks.forEach((item) => {
-    item.callback(value, item.container, item.layout, item.limit, item.lang);
-  });
-});
+(function ($) {
+  $(function() {
+    MyYoutubeRecommendationStudy.loadVideos(my_yt_rec_s_ajax.url).then((value) => {
+      MyYoutubeRecommendationStudy.listCallbacks.forEach((item) => {
+        item.callback(value, item.container, item.layout, item.limit, item.lang);
+      });
+    });
+  })
+})(jQuery);
